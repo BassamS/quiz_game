1e3b20ec4dbe31fb0d0fbbf7120727fec5f94444
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-02 14:56:43 +0200

    final commit

--- quiz_game.py
+++ quiz_game.py
@@ -1,40 +1,46 @@
 print("Welcome to my computer quiz!")
 
-playing = input("Do you want to play? ")
+playing = input("Do you know about computer hardware? You want to play (yes/no)? ")
 
 if playing.lower() != "yes":
-  quit
+  quit()
 
 print("Okay! Let's play :)")
 score = 0
 
-answer = input("What does CPU stand for? ")
+answer = input("\nWhat does CPU stand for? ")
 if answer.lower() == "central processing unit":
   score += 1
-  print("Correct!")
+  print(f"\nCorrect! Your score now is: {score}")
 else:
   print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
   score += 1
-  print("Correct!")
+  print(f"\nCorrect! Your score now is: {score}")
 else:
   print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
   score += 1
-  print("Correct!")
+  print(f"\nCorrect! Your score now is: {score}")
 else:
   print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply unit":
   score += 1
-  print("Correct!")
+  print(f"\nCorrect! Your score now is: {score}")
+else:
+  print("Incorrect!")
+answer = input("What does SSD stand for? ")
+if answer.lower() == "solid state drive":
+  score += 1
+  print(f"\nCorrect! Your score now is: {score}")
 else:
   print("Incorrect!")
 
 print(f"You got {score} questions correct!")
-print(f"You got {(score / 4) * 100} %.")
+print(f"You got {(score / 5) * 100} %.")
